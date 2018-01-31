bdd34ac4b8ff495433765ade6bc88396da12b261
Vincent <vincentmohr0909@gmail.com>
2018-01-31 10:49:11 +0100

    mannschaft

---
+++ src/com/example/helloworl/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.example.helloworl;
+
+
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+int x;
+        System.out.println("Mein Praktikum bei Chefkoch.de");
+   x = Kon.readInt();
+
+        if(x==0){
+
+            System.out.print("Wer bin ich und wieso wollte ich mein Praktikum bei hier machen?:");
+        }
+    }
+}
+
+
+
+
+
+
